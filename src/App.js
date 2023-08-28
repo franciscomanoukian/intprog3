dfbd38a952e25325b2a4d0a819220c5c1b92a04e
franciscomanoukian <fmanoukian@udesa.edu.ar>
2023-08-28 11:23:25 -0300

    Header footer y css general

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from "react";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <p>HOLA</p>
-    
+      <Footer/>
     </div>
   );
 }
